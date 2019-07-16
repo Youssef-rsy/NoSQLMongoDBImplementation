746d1c98ec784157cee2d2bf69c96308da1d0f75
youssef-rsy <youssef.rsy@gmail.com>
2019-07-16 11:54:35 +0100

    initialize project

---
+++ src/main/java/learning/db/nosql/NoSQLMongoDBImplementation/NoSqlMongoDbImplementationApplication.java
@@ -0,0 +1,13 @@
+package learning.db.nosql.NoSQLMongoDBImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlMongoDbImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSqlMongoDbImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learning/db/nosql/NoSQLMongoDBImplementation/NoSqlMongoDbImplementationApplicationTests.java
@@ -0,0 +1,16 @@
+package learning.db.nosql.NoSQLMongoDBImplementation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoSqlMongoDbImplementationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
